c0aa8ff8a49d8ef62468614237721f7d1e636dd4
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2025-03-05 19:12:10 +0000

    Scaffold core module structure

---
+++ liquidmind.py
@@ -0,0 +1,16 @@
+# liquidmind.py
+"""
+Main module for LiquidMind application.
+"""
+
+import argparse
+import logging
+import os
+import sys
+from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class LiquidMindCore:
